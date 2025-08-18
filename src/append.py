cbdd3727e3b5b6ac161c44196486f1f4f50a0f90
Tibor <steinecketibor@gmail.com>
2025-08-18 20:42:51 +0200

    Renamed variable add_newline
    because the parameter of the function already states that it is a list of strings
    'text' because the actual list given into the function is also named 'text'

--- src/append.py
+++ src/append.py
@@ -1,9 +1,9 @@
 
 # Takes list as a parameter because the program stores parameters for "-q" in a list
-def add_newline(list: list[str]): 
-    for i in range(0, len(list)): 
-        list[i] = list[i] + "\n"
-    return list
+def add_newline(text: list[str]): 
+    for i in range(0, len(text)): 
+        text[i] = text[i] + "\n"
+    return text
 
 
 def append_list_to_textfile(text: list[str], filepath: str):
