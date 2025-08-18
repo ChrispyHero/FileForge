9a02e57b7bd76110f18eada57fa671856eb781c6
Tibor <steinecketibor@gmail.com>
2025-08-18 21:07:20 +0200

    Added fileforge.py
    This is the main script.
    seperated the parse and main function for speration of concerns

---
+++ src/fileforge.py
@@ -0,0 +1,51 @@
+import argparse
+import sys 
+import textwrap
+
+
+def parse() -> argparse.Namespace: 
+    """
+    Function to parse the arguments
+    """
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description=textwrap.dedent("""\
+            Github link: 
+            https://github.com/ChrispyHero/FileForge""")
+    )
+
+
+    # The quick flag. For entering lines of text quickly. 
+    # Example: py fileforge.py -q "This text will be appended to a file"
+    parser.add_argument(
+        "-q",
+        "--quick",
+        nargs="+",
+        type=str,
+        metavar="ITEM",
+        help="Gathers all subsequent items into a list. " \
+        "To include spaces within a single item, enclose it in quotes.",
+        dest="quick"
+    )
+
+    # Prints the help message if no flags are given
+    if len(sys.argv) == 1:
+        parser.print_help(sys.stderr)
+        sys.exit(1)
+
+    args = parser.parse_args()
+    
+    return args
+
+
+def main(args: argparse.Namespace):
+    """
+    Function to execute the logic behind each flag given by the user
+    """
+    if args.quick is not None:
+        print("Your input:", args.quick)
+
+
+if __name__ == "__main__":
+    args = parse()
+    main(args)
