c2def756de12e6c4eb01fbd610744842cb583070
Tibor <steinecketibor@gmail.com>
2025-08-23 12:26:46 +0200

    Added safety and parts of json deserialization
    Added more safety to the loading_data_from_json func to handle errors more cleanly and make debugging easier
    
    Added data deserialization for target_file_path
    It does not handle empty strings because the user needs to be asked for the path if the string is empty.
    This will be done by different code

--- src/data.py
+++ src/data.py
@@ -1,8 +1,18 @@
 import json
-
+import sys
 
 def load_data_from_json(file_path:str):
-    with open(file_path) as file:
-        data = json.load(file)
-    return data
-
+    try: 
+        with open(file_path) as file:
+            json_data = json.load(file)
+        return json_data
+    except FileNotFoundError as e: 
+        print(f"There is a problem with the json file: {e}")
+    
+def load_target_file_path(json_data): 
+    data_dict = dict(json_data) 
+    target_path = data_dict.get("target_file_path", None)
+    if target_path is None: 
+        raise KeyError
+    return target_path
+        
